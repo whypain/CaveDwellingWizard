ca263c82cbf7d0d4f262716cd29fd1bd5fc0150b
whypain <painingit@gmail.com>
2025-11-27 23:02:05 +0700

    feat: hook up goal with end level

--- Assets/_Main/Interactables/Objects/Goal.cs
+++ Assets/_Main/Interactables/Objects/Goal.cs
@@ -2,9 +2,9 @@ using UnityEngine;
 
 public class Goal : InteractiveArea
 {
-    public override void InternalInteract(Player player)
+    public override void InternalInteract(Player _)
     {
+        LevelManager.Instance.EndLevel();
         Debug.Log("Goal reached!");
-        // LevelManager.Instance.EndLevel();
     }
 }
